bb0cabd5c007246b6747e3f7a97901083ec8138f
Zach <zach@basebridge.com>
2020-01-23 15:45:54 -0400

    8 - added action creators and reducers for streams

---
+++ 8-streams/client/src/reducers/streamReducer.js
@@ -0,0 +1,27 @@
+import _ from 'lodash'
+import { EDIT_STREAM, FETCH_STREAMS, FETCH_STREAM, CREATE_STREAM, DELETE_STREAM } from "../actions/types"
+
+const streamReducer= (state={}, action) => {
+    switch(action.type) {
+        case FETCH_STREAMS:
+            return { ...state, ..._.mapKeys(action.payload, 'id') }
+        case FETCH_STREAM:
+            return { ...state, [action.payload.id]: action.payload }
+        case CREATE_STREAM:
+            return { ...state, [action.payload.id]: action.payload }
+        case EDIT_STREAM:
+            // Regular Syntax: 
+            // const newState = { ...state }
+            // newState[action.payload.id] = action.payload
+            // return newState
+
+            // Key Interpolation Syntax:
+            return { ...state, [action.payload.id]: action.payload }
+        case DELETE_STREAM:
+            return _.omit(state, action.payload)
+        default: 
+            return state
+    }
+}
+
+export default streamReducer
