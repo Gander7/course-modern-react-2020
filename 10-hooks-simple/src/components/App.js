4f7f601bd7c31e867e5c824f55d29bc9e0452f6b
Zach <zach@basebridge.com>
2020-01-27 12:41:05 -0400

    10 - App with buttons as a class

--- 10-hooks-simple/src/components/App.js
+++ 10-hooks-simple/src/components/App.js
@@ -1,11 +1,27 @@
 import React from 'react'
 
-const App = () => {
-    return (
-        <div className="ui container">
-            App
-        </div>
-    )
+class App extends React.Component {
+    state = { resource: 'posts' }
+
+    render() {
+        return (
+            <div className="ui container">
+                <div>
+                    <button
+                        className="ui button primary" 
+                        onClick={() => this.setState({resource: 'posts'})}>
+                        Posts
+                    </button>
+                    <button
+                        className="ui button primary"
+                        onClick={() => this.setState({resource: 'todos'})}>
+                        Todos
+                    </button>
+                </div>
+                {this.state.resource}
+            </div>
+        )
+    }
 }
 
 export default App
